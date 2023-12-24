bf686a6407af8a98c92a5d0a620ed4c6f9c7da74
M V SIVAKUMAR <mvsk2k@gmail.com>
2023-12-24 18:54:09 +0530

    firstversion

---
+++ main.py
@@ -0,0 +1,82 @@
+"""
+to Run this App, we need to say python main.py
+in terminal
+"""
+
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.animation import Animation
+from hoverable import HoverBehavior
+from kivy.uix.image import Image
+from kivy.uix.behaviors import ButtonBehavior
+import json, glob, random
+from datetime import datetime
+from pathlib import Path
+
+Builder.load_file('design.kv')
+
+class LoginScreen(Screen):
+    def sign_up(self):
+        self.manager.current = "sign_up_screen"
+
+    def login(self, uname, pword):
+        with open("users.json", ) as file:
+            users = json.load(file)
+        if uname in users and users[uname]['password'] == pword:
+            self.manager.current = "login_screen_success"
+        else:
+            anim = Animation(color = (0.6, 0.7, 0.1, 1))
+            anim.start(self.ids.login_wrong)
+            self.ids.login_wrong.text = "Wrong user name or Password !"
+
+
+class RootWidget(ScreenManager):
+    pass
+
+class SignUpScreen(Screen):
+    def add_user(self, uname, pword):
+        with open("users.json", ) as file:
+            users = json.load(file)
+
+        users[uname] = {'username': uname, 'password': pword,
+                        'created': datetime.now().strftime("%Y-%m-%d %H-%M-%S")}
+
+        with open("users.json", 'w') as file:
+            json.dump(users, file)
+
+        print(users)
+        self.manager.current = "sign_up_screen_success"
+
+class  SignUpScreenSuccess(Screen):
+    def go_to_login(self):
+        self.manager.transition.direction = 'right'
+        self.manager.current = "login_screen"
+
+
+class  LoginScreenSuccess(Screen):
+    def log_out(self):
+        self.manager.transition.direction = 'right'
+        self.manager.current = "login_screen"
+    def get_quote(self, feel):
+        feel = feel.lower()
+        available_feeling = glob.glob("quotes/*.txt")
+        available_feeling = [Path(filename).stem for filename in
+                             available_feeling]
+        if feel in available_feeling:
+            with open(f"quotes/{feel}.txt", 'r', encoding='utf-8') as f:
+                quotes = f.readlines()
+            #print(quotes)
+            self.ids.quote.text = random.choice(quotes)
+        else:
+            self.ids.quote.text = "Try another feeling"
+
+class ImageButton(ButtonBehavior, HoverBehavior, Image):
+    pass
+
+class MainApp(App):
+    def build(self):
+        return RootWidget()
+
+if __name__ == "__main__":
+    MainApp().run()
